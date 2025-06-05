cbfb23ec6500894c91f986fdd945339c0d38cb4e
hitesh <hiteshv253@gmail.com>
2025-06-05 17:14:47 +0530

    club master, desgina nd fix some codes

---
+++ database/factories/DgTeamFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\DgTeam;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DgTeamFactory extends Factory {
+
+      protected $model = DgTeam::class;
+
+      public function definition() {
+            return [
+                      'name' => $this->faker->name,
+                      'email' => $this->faker->unique()->safeEmail,
+                      'designation' => $this->faker->jobTitle,
+                      'phone' => $this->faker->phoneNumber,
+                      'address' => $this->faker->address,
+                      'is_active' => true,
+                      'is_create_by' => 1,
+            ];
+      }
+}
